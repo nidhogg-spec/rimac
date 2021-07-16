a11bd0a7893851e6d707df1d3f766d62cdc4e631
Carlos Schiavonne <cfschiavonn@gmail.com>
2021-07-15 23:18:51 -0500

    Header Terminado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
